5be6c4355edb240b51fe7d1d74da8395bf8ce35f
Mike Lee <mikecovlee@163.com>
2025-01-17 09:23:55 +0800

    [doc] update requirements

--- misc/install-extra-linux.sh
+++ misc/install-extra-linux.sh
@@ -1,4 +1,4 @@
 # Install extra requirements
 pip3 install ninja
-pip3 install bitsandbytes==0.43.1
-pip3 install flash-attn==2.5.8 --no-build-isolation
+pip3 install bitsandbytes==0.44.1
+pip3 install flash-attn==2.6.3 --no-build-isolation
